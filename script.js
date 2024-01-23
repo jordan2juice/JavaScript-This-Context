7a64e5804759d5b5329d47e796c1870ea317c103
jordan toujouse <jtoujo1@gmail.com>
2024-01-22 21:42:37 -0600

    Corrected the script for the buttons

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
-message1: 'The first button was clicked'
-message2: 'The second button was clicked'
-message3: 'The third button was clicked'
-
-displayMessage1 function () {
+const messageHandler = {
+    message1: "The first button was clicked",
+    message2: 'The second button was clicked',
+    message3: 'The third button was clicked',
+//This is referring to the messageHandler//
+displayMessage1: function () {
     this.displayMessage1 (this.message1);
-}
-displayMessage2 function () {
-    this.displayMessage2(this.message2)
-}
-displayMessage3 function () {
-    this.displayMessage3(this.message3)
-}
-displayMessage function () {
-    const p = document.createElement('p')
-    
-}
+},
+displayMessage2: function () {
+    this.displayMessage2(this.message2);
+},
+displayMessage3: function () {
+    this.displayMessage3(this.message3);
+},
+displayMessage: function () {
+    const p = document.createElement('p');
+    p.textContent = message;
+    document.body.appendChild(p);
+ },
+};
+
+btn1 = document.getElementById('button-1').addEventListener('click', messageHandler.displayMessage1.bind(messageHandler));
+btn2 = document.getElementById('button-2').addEventListener('click', messageHandler.displayMessage2.bind(messageHandler));
+btn3 = document.getElementById('button-3').addEventListener('click', messageHandler.displayMessage3.bind(messageHandler));
