deffbb1eaa714cfa260ce1b53c0a485257e8704c
jordan toujouse <jtoujo1@gmail.com>
2024-01-22 21:02:05 -0600

    started writing javascipt for the messages

---
+++ script.js
@@ -0,0 +1,17 @@
+message1: 'The first button was clicked'
+message2: 'The second button was clicked'
+message3: 'The third button was clicked'
+
+displayMessage1 function () {
+    this.displayMessage1 (this.message1);
+}
+displayMessage2 function () {
+    this.displayMessage2(this.message2)
+}
+displayMessage3 function () {
+    this.displayMessage3(this.message3)
+}
+displayMessage function () {
+    const p = document.createElement('p')
+    
+}
