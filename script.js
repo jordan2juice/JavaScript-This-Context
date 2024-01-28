c070f374db62eb53bb497c352644d2bedd8a5d80
jordan toujouse <jtoujo1@gmail.com>
2024-01-28 12:42:39 -0600

    added buttons and more script for the new buttons with .this

--- script.js
+++ script.js
@@ -20,9 +20,7 @@ const messageHandler = {
     document.body.appendChild(p);
   },
 };
-const displayMessage1 = messageHandler.displayMessage1.bind(messageHandler)
-
-
+const displayMessage1 = messageHandler.displayMessage1.bind(messageHandler);
 
 let btn1 = document
   .getElementById("button-1")
@@ -42,3 +40,40 @@ let btn3 = document
     "click",
     messageHandler.displayMessage3.bind(messageHandler)
   );
+
+const person = {
+  message1: "Hi I'm John",
+  message2: "I am 30 years old",
+  message3: "I work as a developer",
+
+  displayMessage1: function () {
+    this.displayMessage1(this.message1);
+  },
+  displayMessage2: () => {
+    this.displayMessage2(this.message2);
+  },
+  displayMessage3: function () {
+    this.displayMessage3(this.message3);
+  },
+  displayMessage: function (message) {
+    // function implementation
+    const p = document.createElement("p");
+    p.textContent = message;
+    document.body.appendChild(p);
+  },
+};
+
+let pBtn = document
+  .getElementById("button-4")
+  .addEventListener(
+    "click",
+    messageHandler.displayMessage1.bind(messageHandler)
+  );
+// With this binding messageHandler this is attached to the messageHandler with message1 property values
+let pBtn2 = document
+  .getElementById("button-5")
+  .addEventListener(
+    "click",
+    messageHandler.displayMessage2.bind(messageHandler)
+  );
+// This will create an error because there are no message3 property in messageHandler object, this will not work because
